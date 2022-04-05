4ffd85567b8ae42e329c73d23704041df5581772
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-05 21:10:40 +0100

    Created seperate component for header and added logo image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./components/Header";
 
 import Bookings from "./Bookings";
 import "./App.css";
@@ -6,8 +7,8 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
       <Bookings />
+      <Header />
     </div>
   );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <img
+        className="App-logo"
+        src="https://as2.ftcdn.net/v2/jpg/00/82/58/01/1000_F_82580196_bfodUsOwMwOaMF3O6qkUsB7HctfpqtHI.jpg"
+      />
+      <header className="App-header">CYF Hotel</header>
+    </div>
+  );
+};
+
+export default Header;
